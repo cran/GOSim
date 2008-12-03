c25cdc0593aab6d3b5b9cb5da8608e73af10e4ec
Holger Froehlich <h.froehlich@dkfz.de>
2008-12-03 00:00:00 +0000

    version 1.1.5.4

--- R/GOenrichment.R
+++ R/GOenrichment.R
@@ -26,22 +26,40 @@
 # }
 
 # perform a GO gene set enrichment analysis for a specific cluster using topGO
-GOenrichment = function(genesOfInterest, allgenes, cutoff=0.01, method=c("weightCount","elimCount")){
+GOenrichment = function(genesOfInterest, allgenes, cutoff=0.01){
 	if(!require(topGO) | !require(annotate))		
 		stop("Packages topGO and annotate required for function analyzeCluster")	
-	ontology = get("ontology", envir=GOSimEnv)	
+	ontology = get("ontology", envir=GOSimEnv)		
+	topgenes = function(allScore){				
+		return(names(allgenes) %in% names(genesOfInterest))		
+	}
+	if(class(allgenes) == "character" & class(genesOfInterest) == "character"){
+		genelist = factor(as.integer(allgenes %in% genesOfInterest))
+		names(genelist) = as.character(allgenes)
+		geneIDs = names(genelist)		
+		method = "elimCount"
+	}
+	else if(class(allgenes) == "numeric" & class(genesOfInterest) == "numeric"){
+		geneIDs = as.character(names(allgenes))		
+		method = "weightCount"
+	}
+	else
+		stop("Parameters 'allgenes' and 'genesOfInterest' have either to be character vectors of Entrez gene IDs or vectors of p-values named with Entrez gene IDs")
+
 	gomap <- get("gomap",env=GOSimEnv)	
-	anno <- gomap[as.character(allgenes)]
+	anno <- gomap[geneIDs]
 	goterms = lapply(anno, function(x) sapply(x, function(y) y$Ontology == ontology))
 	goterms = goterms[!is.na(names(goterms))]
 	if(length(goterms) == 0)
 		stop("No GO information available for these genes!")
 	goterms <-sapply(goterms, function(x) names(x[which(x)]))
-	goterms <- goterms[sapply(goterms,length) > 0]	
-	geneList <- factor(as.integer(allgenes %in% genesOfInterest))
-	names(geneList) <- allgenes
-	GOdata = new("topGOdata", ontology = ontology, allGenes = geneList, annot = annFUN.gene2GO, gene2GO = goterms)  
-	test.stat <- new(method[1], testStatistic = GOFisherTest, name = "Fisher test", cutOff = cutoff) 
+	goterms <- goterms[sapply(goterms,length) > 0]			
+
+	if(method == "elimCount")
+		GOdata = new("topGOdata", ontology = ontology, allGenes = genelist, annot = annFUN.gene2GO, gene2GO = goterms)  
+	else
+		GOdata = new("topGOdata", ontology = ontology, allGenes = allgenes, geneSel=topgenes, annot = annFUN.gene2GO, gene2GO = goterms)  	
+	test.stat <- new(method, testStatistic = GOFisherTest, name = "Fisher test", cutOff = cutoff) 
 	res = getSigGroups(GOdata, test.stat) 
 	sigterms = score(res)[score(res) < cutoff]
 	sigGOs = names(sigterms)
